b2ff613591a5949cbc95388316c263ec95746da2
mohammed Elsdody <31186483+mohamedma872@users.noreply.github.com>
2024-09-07 05:55:40 +0300

    stable release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,8 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(project(":HijriDatePickerLib"))
+   // implementation(project(":HijriDatePickerLib"))
 
-   // implementation ("com.github.mohamedma872:HijriDatePickerLib:1.0.0")
+    implementation ("com.github.mohamedma872:HijriDatePickerPlus:Development-SNAPSHOT")
 
 }
--- app/src/main/java/com/sdody/HijriDatePickerPlusApplication/MainActivity.kt
+++ app/src/main/java/com/sdody/HijriDatePickerPlusApplication/MainActivity.kt
@@ -1,36 +1,10 @@
 package com.sdody.HijriDatePickerPlusApplication
-import android.icu.util.IslamicCalendar
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.Dialog
 import com.sdody.hijridatepickerpluslib.HijriDatePickerButton
-import java.util.Calendar
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
